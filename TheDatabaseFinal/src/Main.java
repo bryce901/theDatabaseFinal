bb219f962b5330bd163d750ec14321d20c211ab3
bryce901 <36871541+bryce901@users.noreply.github.com>
2019-05-07 18:54:46 -0400

    Add files via upload

---
+++ TheDatabaseFinal/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    static final String JBDC_DRIVER="com.mysql.jbdc.Driver";
+    static final String DB_URL="jdbc:mysql://35.192.17.149:3306/MasterDB";
+    static final String USER="ryanbryce";
+    static final String PASS="poop";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
